f8501a32ec1bf7bcb0ea2b72d49bddbb9b9c670f
leowang707 <123796266+leowang707@users.noreply.github.com>
2024-05-28 16:30:20 +0800

    Add files via upload

---
+++ Docker/docker_join.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Typical usage: ./join.bash subt
+#
+
+BASH_OPTION=bash
+
+IMG=argnctu/detr:gpu
+
+xhost +
+containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
+docker exec -it \
+    --privileged \
+    -e DISPLAY=${DISPLAY} \
+    -e LINES="$(tput lines)" \
+    ${containerid} \
+    $BASH_OPTION
+xhost -
