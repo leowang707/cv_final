bd87f83556840bd1ceb806e56b59f35e358c78a4
leowang707 <123796266+leowang707@users.noreply.github.com>
2024-06-03 17:41:49 +0800

    Add files via upload

--- jupyter_notebook.sh
+++ jupyter_notebook.sh
@@ -1,7 +1,7 @@
 jupyter notebook \
     --NotebookApp.allow_origin='https://colab.research.google.com' \
     --NotebookApp.port_retries=0 \
-    --notebook-dir /home/leowang707/detr_seg \
+    --notebook-dir /home/leowang707/cv_final \
     --port=8888 \
     --allow-root \
     --no-browser \
